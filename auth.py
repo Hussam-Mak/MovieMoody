d30860d83ad212ed299c45b903108d32e249d566
Hussam Makhoul <hussammakhoul2733@gmail.com>
2025-07-01 18:54:21 -0500

    adding auth and env

--- auth.py
+++ auth.py
@@ -2,6 +2,6 @@
 # auth.py - Simulated GitHub OAuth (for CLI simplicity)
 
 def authenticate():
-    print("Please log in with your GitHub username.")
-    username = input("GitHub Username: ").strip()
+    print("Please log in with your username.")
+    username = input("Username: ").strip()
     return username
